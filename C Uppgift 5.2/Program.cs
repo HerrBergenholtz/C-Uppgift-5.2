06abb9784737dc15d96d7994d0d81c1381f8d2f3
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-22 10:22:18 +0100

    Add project files.

---
+++ C Uppgift 5.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Uppgift_5._3
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Hur många tal vill du skriva in?");
+                int antal = int.Parse(Console.ReadLine());
+                int[] tal = new int[antal];
+                Console.WriteLine($"Skriv in {antal} heltal");
+                for (int i = 0; i < tal.Length; i++)
+                {
+                    tal[i] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("Detta är de tal som du skrev in:");
+                
+                for (int i = 0;i < tal.Length; i++)
+                {
+                    Console.WriteLine($"Tal {i + 1}: {tal[i]}");
+                }
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Du gav inte rätt input, försök igen");
+                Main();
+            }
+        }
+    }
+}
